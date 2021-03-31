bbd719f15d5f2ed731a748b9308c3a5cc99bf100
V203 <vuyisamr203@gmail.com>
2021-03-31 08:13:13 +0200

    manjaro

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,7 +1,11 @@
 describe('The greet function', function(){
-
+                
     it('should greet Andrew correctly', function(){
         assert.equal('Hello, Andrew', greet('Andrew'));
     });
     
+    var name ;
+    it('Should greet based on taking in from variable', function(){
+        assert.equal(greet(name),"Hello, "+name)
+    });
 });
--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,5 +1,9 @@
 describe("Years ago function",function(){
-	it("the exesice should equal 45",function(){
+	it("the exesice should equal 2000",function(){
 		assert.equal(yearsAgo(2000),21)
+	})
+
+	it("expected to equal 21",function(){
+		assert.equal(yearsAgo(21),2000)
 	})
 })
