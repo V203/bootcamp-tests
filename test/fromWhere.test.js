7ddc1468d7b60ee045cace450273f0fe7d215ba8
v203 <vuyisamr203@gmail.com>
2021-04-07 11:25:32 +0200

    codex

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -1,13 +1,13 @@
 describe("fromWhere function",function(){
-    it("Expected CY to equal Bellville",function(){
+    it("Expected CY 000 343 to equal Bellville",function(){
         assert.equal(fromWhere("CY 000 343"),"Bellville")
     })
 
-    it("Expected from where to be a function and nothing else",function(){
-        assert.isFunction(fromWhere,"Paarl")
+    it("Expected CJ 768 989 to equal bellville",function(){
+        assert.equal(fromWhere("CJ 768 989"),"Paarl")
     })
 
-    it("Expected CA to equal Cape Town",function(){
-        assert.isString("CA 757 987","Cape Town")
+    it("Expected CA 757 987 to equal Cape Town",function(){
+        assert.equal(fromWhere("CA 757 987"),"Cape Town")
     })
 })
